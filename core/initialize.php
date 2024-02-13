52c71b6a13ca519aae436c1441a32668d03adc39
jhembe <jhembe202@gmail.com>
2024-02-13 08:05:32 +0300

    add initial post route

--- core/initialize.php
+++ core/initialize.php
@@ -1,7 +1,7 @@
 <?php
 
     defined('DS')? null : define('DS',DIRECTORY_SEPARATOR);
-    defined('SITE_ROOT') ? null : define('SITE_ROOT',DS.'opt'.DS.'lampp'.DS.'htdocs'.DS.'apitesting');
+    defined('SITE_ROOT') ? null : define('SITE_ROOT',DS.'xampp'.DS.'htdocs'.DS.'restapi-test');
     //opt/lampp/htdocs/apitesting
     defined('INC_PATH') ? null : define('INC_PATH',SITE_ROOT.DS.'includes');
     defined('CORE_PATH') ? null : define('CORE_PATH',SITE_ROOT.DS.'core');
