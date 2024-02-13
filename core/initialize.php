492ef687e146b99b2ba71b6392b50ce756128446
jhembe <jhembe202@gmail.com>
2024-02-13 14:16:19 +0300

    ADDED : Site Root definition for Windows and Linux

--- core/initialize.php
+++ core/initialize.php
@@ -1,11 +1,15 @@
 <?php
 
     defined('DS')? null : define('DS',DIRECTORY_SEPARATOR);
+
+    // Site Root definition for Linux users.
     defined('SITE_ROOT') ? null : define('SITE_ROOT',DS.'opt'.DS.'lampp'.DS.'htdocs'.DS.'restapi-test');
-    //opt/lampp/htdocs/apitesting
+
+    // Site Root definition for windows users.
+    // defined('SITE_ROOT') ? null : define('SITE_ROOT',DS.'xampp'.DS.'htdocs'.DS.'restapi-test');
+
     defined('INC_PATH') ? null : define('INC_PATH',SITE_ROOT.DS.'includes');
     defined('CORE_PATH') ? null : define('CORE_PATH',SITE_ROOT.DS.'core');
-
 
     // load the config file first
     require_once(INC_PATH.DS."config.php");
